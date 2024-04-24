c9ef7ca6679ccafd6caf7d893a051eabf8176c09
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2024-04-24 16:14:23 +0300

    feat: add main section

---
+++ test-project/src/components/MainSection/MainSection.tsx
@@ -0,0 +1,11 @@
+import "./MainSection.scss";
+const MainSection = () => {
+  return (
+    <section className="main-section">
+      <div className="main-section__right-bg"></div>
+      <div className="main-section__left-bg"></div>
+    </section>
+  );
+};
+
+export default MainSection;
